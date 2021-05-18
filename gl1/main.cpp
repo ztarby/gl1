89fc8d320a135c60fa3996e3e8bdbd2ad4418c2b
Ztarby Xtar <ztarbyxtar@gmail.com>
2021-05-18 17:42:12 -0500

    shader course end

--- gl1/main.cpp
+++ gl1/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<vector>
 
 #include"VBO.h"
 #include"VAO.h"
@@ -9,6 +10,8 @@
 
 
 int main(){
+	
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
@@ -24,12 +27,12 @@ int main(){
 
 	 GLfloat vertices[] =
 	 {
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.10f, 0.2f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.13f, 0.4f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     0.16f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.19f, 0.8f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.22f, 0.10f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.25f, 0.12f,  0.02f  // Inner down
 	 };
 	 GLuint indices[] =
 	 {
@@ -43,6 +46,7 @@ int main(){
 	 glViewport(0,0,800,800);
 
 	 Shader shaderProgram("default.vert", "default.frag");
+     
 
 	 VAO VAO1;
 	 VAO1.Bind();
@@ -58,7 +62,7 @@ int main(){
 	 VBO1.Unbind();
 	 EBO1.Unbind();
 
-
+	 GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	 glfwSwapBuffers(window);
 
@@ -66,6 +70,8 @@ int main(){
 		 glClearColor(0.08f, 0.13f, 0.17f, 1.0f);
 		 glClear(GL_COLOR_BUFFER_BIT);
 		 shaderProgram.Activate();
+		 glUniform1f(uniID, 0.4f);
+
 		 VAO1.Bind();
 		 glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		 glfwSwapBuffers(window);
